6e63774d45f3626ab1b5963adb1489a20cc863d2
agayev169 <agayev169@gmail.com>
2018-09-19 22:52:16 +0400

    Modifying train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, BatchNormalization, SpatialDropout2D
+# from tensorflow.keras import optimizers
 import numpy as np
 
 from tensorflow.keras.datasets import cifar10
@@ -22,24 +23,25 @@ model = Sequential()
 model.add(Conv2D(128, (3, 3), input_shape = X_train.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.1))
+model.add(SpatialDropout2D(0.2))
 model.add(BatchNormalization())
 
 model.add(Conv2D(128, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.1))
+model.add(SpatialDropout2D(0.2))
 model.add(BatchNormalization())
 
 model.add(Flatten())
 model.add(Dense(256))
 model.add(Activation('relu'))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 model.add(BatchNormalization())
 
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
+# opt = optimizers.Adam(learning_rate = 0.0001)
 model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 model.fit(X_train, y_train, epochs = 20, batch_size = 128, 
 	validation_data = (X_test, y_test))
