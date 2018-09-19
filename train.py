835788273925ee602ce9fa84f4bd5f5650862998
agayev169 <agayev169@gmail.com>
2018-09-19 13:01:14 +0400

    Adding new files

---
+++ train.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Activation, Flatten, Dropout
+import numpy as np
+
+from tensorflow.keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+X_train = X_train.reshape(X_train.shape[0], 32, 32, 3)
+X_test = X_test.reshape(X_test.shape[0], 32, 32, 3)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+Y_train = tf.keras.utils.to_categorical(y_train, 10)
+Y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+model = Sequential()
+
+model.add(Conv2D(128, (2, 2), input_shape = X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(128, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(X_train, y_train, epochs = 20, batch_size = 128, 
+	validation_data = (X_test, y_test))
